aff291aa9be3bd4d24b65cd19c1a0b3a333b61b3
JBennett <jbennett@ccistudios.com>
2010-09-23 14:33:59 -0400

    Basic layout of sponsor pages

--- com_calendar/administrator/components/com_calendar/views/sponsor/tmpl/form.php
+++ com_calendar/administrator/components/com_calendar/views/sponsor/tmpl/form.php
@@ -1,1 +1,30 @@
-sponsor
+<?php defined('KOOWA'); ?>
+
+<form action="<?=@route('id='.$sponsor->id)?>" method="post" id="mainform" name="adminForm" class="adminform">
+<div class="width66 left">
+<fieldset>
+	<legend><?=@text('Sponsor Details')?></legend>
+
+	<label for="title_field" class="mainlabel"><?=@text('Title')?>:</label>
+	<input type="text" id="title_field" name="title" value="<?=$sponsor->title?>" /><br/>
+	
+	<label for="description_field" class="mainlabel"><?=@text('Description')?></label>
+	editor<br/>
+	
+	<label for="date_field" class="mainlabel"><?=@text('Date')?>:</label>
+	<input type="text" id="date_field" name="date" value="<?=$sponsor->date?>" /><br/>
+	
+	user<br/>
+	invoice<br/>
+	
+	
+	<label for="image_field" class="mainlabel"><?=@text('Image')?>:</label>
+	<input type="text" id="image_field" name="image" value="<?=$sponsor->image?>" /><br/>
+</fieldset>
+</div>
+
+<div class="clr"></div>
+</form>
+
+
+<style src="media://com_default/css/form.css" />
--- com_calendar/administrator/components/com_calendar/views/sponsors/tmpl/default.php
+++ com_calendar/administrator/components/com_calendar/views/sponsors/tmpl/default.php
@@ -1,1 +1,63 @@
-sponsors
+<?php defined('KOOWA') or die; ?>
+
+<form action="<?=@route()?>" method="get">
+<table class="adminlist">
+	<thead>
+		<tr>
+			<th width="5">#</th>
+			<th width="20" align="center">&nbsp;</th>
+			<th width="20" align="center"><?=@helper('grid.sort', array('state'=>$state, 'column'=>'date'))?></th>
+			<th width="150" align="center"><?= @text('Preview')?></th>
+			<th><?=@helper('grid.sort', array('state'=>$state, 'column'=>'title'))?></th>
+			<th width="75"><?=@text('Invoice')?></th>
+			<th width="50" align="center"><?=@helper('grid.sort', array('state'=>$state, 'column'=>'enabled'))?></th>
+			<th width="20" align="center"><?=@helper('grid.sort', array('state'=>$state, 'column'=>'id'))?></th>
+		</tr>
+	</thead>
+	
+	<tfoot>
+		<tr>
+			<td colspan="20" align="center">
+				<?=@helper('admin::com.default.helper.paginator.pagination', array('state'=>$state, 'total'=>$total))?>
+			</td>
+	</tfoot>
+	
+	<tbody>
+		<? $i = 1;
+		foreach($sponsors as $sponsor): ?>
+		<tr>
+			<td align="center"><?=$i?></td>
+			<td align="center">&nbsp;</td>
+			<td align="center"><?=$sponsor->date?></td>
+			<td align="center">&nbsp;</td>
+			<td>
+				<a href="<?=@route('view=sponsor&id='.$sponsor->id)?>">
+					<?=$sponsor->title?>
+				</a>
+			</td>
+			<td align="center">
+				<? if ($sponsor->calendar_invoice_id): ?>
+				<a href="<?=@route('view=invoice&id='.$sponsor->calendar_invoice_id)?>">
+					<?=@text('Invoice')?>
+				</a>
+				<? else: ?>
+					<?=@text('No Invoice')?>
+				<? endif; ?>
+			</td>
+			<td align="center"><?=@helper('grid.enable', array('row'=>$sponsor))?></td>
+			<td align="center"><?=$sponsor->id?></td>
+		</tr>
+		<? $i++;
+		endforeach; ?>
+		
+		<? if (!count($sponsors)):?>
+		<tr>
+			<td colspan="20" align="center">
+				<?=@text('No sponsors')?>
+			</td>
+		<? endif; ?>
+	</tbody>
+</table>
+</form>
+
+<style src="media://com_default/css/admin.css" /> 
