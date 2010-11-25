3b4e90e7dbb4970159549181c6b760ddd73fe5a6
JBennett <jbennett@ccistudios.com>
2010-11-25 10:08:15 -0500

    Fixed method of retreiving the needed information, fixed issue with providing all details to the view template

--- com_calendar/modules/mod_date/html.php
+++ com_calendar/modules/mod_date/html.php
@@ -6,32 +6,36 @@ class ModDateHtml extends ModDefaultHtml {
 		$date = KRequest::get('get.date', 'date', date('Y-m-d'));
 		$view = KRequest::get('get.view', 'cmd', 'day');
 		
+		$component = JComponentHelper::getComponent('com_calendar');
+		$params = new JParameter($component->params);
+		$blank	= $params->get('available_day_id');
 
 		$day	= substr($date, 8, 2);
 		$month	= substr($date, 5, 2);
 		$year	= substr($date, 0, 4);
 		$month_name = date('F', mktime(0,0,0, $month, 1, $year));
 		
-		if ($view === 'day') {
-			$title = KFactory::tmp('admin::com.calendar.model.days')
-						->set('date', $date)
-						->limit(1)
-						->getList()->current();
-			if ($title) {
-				$title = $title->user_name;
-			} else {
-				$title = "AVAILABLE";
-			}
+		$table = KFactory::tmp('admin::com.calendar.database.table.days');
+		$query = $table->getDatabase()->getQuery()
+				->where('date', '=', $date)
+				->where('calendar_day_id', '=', $blank, 'or')
+				->order('calendar_day_id', 'desc')
+				->limit(2);
+		$result = $table->select($query);		
+				
+		if ((count($result) === 2 && $result->current()->id != $blank) || count($result) === 1) {
+			$today = $result->current();
 		} else {
-			$title = "SPONSOR NAME";
+			$today = $result->next();
 		}
 		
+		$title = $today->dedication;		
 		
 		$this->assign('month_name', $month_name);
 		$this->assign('month', $month);
 		$this->assign('day', $day);
 		$this->assign('year', $year);
-		$this->assign('title', $title);
+		$this->assign('row', $today);
 		parent::display();
 	}
 }
--- com_calendar/modules/mod_date/tmpl/default.php
+++ com_calendar/modules/mod_date/tmpl/default.php
@@ -1,4 +1,4 @@
 <div class="mod-date">
 	<span style="font-size: 65px;"><?= $month_name?> <?= $day ?>, <?= $year ?></span><br />
-	<span style="font-size: 40px;"><?= $title?></span>
+	<span style="font-size: 40px;"><?= $row->dedication ?></span>
 </div>
