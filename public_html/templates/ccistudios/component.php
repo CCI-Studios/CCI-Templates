309fd6622df0f1fe90b0e39da7b5f022955590c4
JBennett <jbennett@ccistudios.com>
2010-12-01 12:52:36 -0500

    Added basic component layout

--- public_html/templates/ccistudios/component.php
+++ public_html/templates/ccistudios/component.php
@@ -1,1 +1,43 @@
-componentf6
+<!DOCTYPE html>
+<?php
+$menu = JSite::getMenu();
+if ($menu)
+    $menu = $menu->getActive();
+if ($menu)
+    $menu = $menu->alias;
+$testing = "true";
+
+?>
+<html>
+<head>
+	<jdoc:include type="head" />
+
+
+<?php if ($testing): ?>
+	<meta name="robots" content="nofollow, noindex" />
+<?php endif; ?>
+
+
+<?php if ($testing): ?>
+	<link rel="stylesheet" type="text/css" href="/templates/<?php echo $this->template ?>/css/960/reset.css" />
+	<!--<link rel="stylesheet" type="text/css" href="/templates/<?php echo $this->template ?>/css/960/960.css" />-->
+	<link rel="stylesheet" type="text/css" href="/templates/<?php echo $this->template ?>/css/960/text.css" />
+	<link rel="stylesheet" type="text/css" href="/templates/<?php echo $this->template ?>/css/forms.css" />
+	<link rel="stylesheet" type="text/css" href="/templates/<?php echo $this->template ?>/css/layout.css" />
+	<link rel="stylesheet" type="text/css" href="/templates/<?php echo $this->template ?>/css/modules.css" />
+	<link rel="stylesheet" type="text/css" href="/templates/<?php echo $this->template ?>/css/style.css" />
+	<link rel="stylesheet" type="text/css" href="/templates/<?php echo $this->template ?>/css/editorStyles.css" />
+	
+	<?php JHtml::_('behavior.mootools')?>
+	<script type="text/javascript" src="/templates/<?php echo $this->template ?>/scripts/dropmenu.js"></script>
+<?php else: ?>
+	<link rel="stylesheet" type="text/css" href="/templates/<?php echo $this->template ?>/css/template.css" />
+<?php endif; ?>
+</head>
+
+<body>
+	<div style="padding: 10px;">
+		<jdoc:include type="component" />
+	</div>
+</body>
+</html>
