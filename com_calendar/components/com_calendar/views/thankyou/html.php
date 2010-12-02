40fc60ccaa8e88b85414fe37922e4e06167af2fe
JBennett <jbennett@ccistudios.com>
2010-12-02 10:22:11 -0500

    Added title

---
+++ com_calendar/components/com_calendar/views/thankyou/html.php
@@ -0,0 +1,11 @@
+<?php
+defined('KOOWA') or die;
+
+class ComCalendarViewThankyouHtml extends ComCalendarViewHtml {
+	
+	public function display() {
+		JFactory::getDocument()->setTitle('Thank You');
+		
+		return parent::display();
+	}
+}
