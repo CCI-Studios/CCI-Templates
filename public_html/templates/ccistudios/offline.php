79a6b23c1674cef5ae27d704ae42578bd8645932
JBennett <jbennett@ccistudios.com>
2010-12-06 08:40:07 -0500

    Updated offline template

--- public_html/templates/ccistudios/offline.php
+++ public_html/templates/ccistudios/offline.php
@@ -1,1 +1,32 @@
-offline
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+	<jdoc:include type="head" />
+	
+	<style type="text/css">
+	.outer {
+		width: 810px;
+		margin: 30px auto 0;
+		text-align: center;
+	}
+	</style>
+</head>
+
+<body>
+	<script type="text/javascript">
+	  var _gaq = _gaq || [];
+	  _gaq.push(['_setAccount', 'UA-XXXXXXXX-X']);
+	  _gaq.push(['_trackPageview']);
+	
+	  (function() {
+	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	  })();
+	</script>
+
+	<div class="outer">
+		<img src="/templates/<?= $this->template?>/images/offline.png" />
+	</div>
+</body>
+</html>
