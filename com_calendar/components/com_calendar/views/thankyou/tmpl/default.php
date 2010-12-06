d8ea30b16cfb445564d37f6ae0550ee962d24da3
JBennett <jbennett@ccistudios.com>
2010-12-06 00:27:25 -0500

    Updated page text

--- com_calendar/components/com_calendar/views/thankyou/tmpl/default.php
+++ com_calendar/components/com_calendar/views/thankyou/tmpl/default.php
@@ -2,6 +2,10 @@
 	<div class="thankyou">
 		<h1>Thank You!</h1>
 		
-		<p>Thank you for your contribution to the Big Sisters of Sarnia Lambton.</p>
+		<p>Thank you your contribution to Big Sisters of Sarnia-Lambton! Your donation will ensure the continued offering of mentoring programs
+			 to girls in Sarnia – and for that, we are incredibly grateful.</p>
+		<p>Be sure to check <a href="/">whatsyourday.com</a> to see who else is supporting Big Sisters - and proudly say your date the next time
+			 you are asked, “What’s Your Day?”!</p>
+		<p><a href="/"><< Go to Today’s Page</a></p>
 	</div>
 </div>
