bb29847b47e404941d2e949c169ad5291b40737e
Jonathan Bennett <jbennett@ccistudios.com>
2011-01-31 14:53:38 -0500

    Structure in place
    more details

---
+++ templates/darren_2011/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<jdoc:include type="head" />
+		 
+		<link href="/templates/darren_2011/css/template.css" rel="stylesheet" />
+	</head>
+	
+	<body>
+		
+		<div id="wrapper">
+			
+			<div id="header"></div>
+			
+			<div id="body">
+				<div id="component">
+					<h1>About us</h1>
+					<p>lorem ipsum</p>
+					<p>more stuff</p>
+				</div>
+				<div id="sidebar">
+					<ul>
+						<li>Services</li>
+						<li>Products</li>
+						<li>Community</li>
+					</ul>
+				</div>
+				
+				<div id="bottom"></div>
+			</div><!-- end body -->
+		
+			<div id="footer">
+				<div class="left">Copyright 2011 Darren</div>
+				<div class="right">Site By CCI Studios</div>
+			</div>
+		</div><!-- end wrapper -->
+	</body>
+</html>
