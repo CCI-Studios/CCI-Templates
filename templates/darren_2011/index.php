7bc78ef38f29f9a6416ad0012ff4414eaf1f45fd
unknown <Darren@.(none)>
2011-02-01 12:56:12 -0500

    finished first template

--- templates/darren_2011/index.php
+++ templates/darren_2011/index.php
@@ -14,19 +14,28 @@
 				<jdoc:include type="modules" name="header" style="xhtml" />
 			</div>
 			
-			<div id="body">
-				<div id="component">
+			<div id="body"><div>
+				<div id="component"
+				<?php if ($this->countModules('sidebar') === 0): ?>
+					class="wide"
+				<?php endif; ?>
+				>
 					<jdoc:include type="component" />
 				</div>
+				
+				<?php if ($this->countModules('sidebar') !== 0): ?>
 				<div id="sidebar">
 					<jdoc:include type="modules" name="sidebar" style="xhtml" />
 				</div>
+				<?php endif; ?>
+				
+				
 				<div class="clear"></div>
 				
 				<div id="bottom">
 					<jdoc:include type="modules" name="bottom" style="xhtml" />
 				</div>
-			</div><!-- end body -->
+			</div></div><!-- end body -->
 		
 			<div id="footer">
 				<div class="left">&copy; Copyright <?php echo date('Y'); ?> Darren</div>
